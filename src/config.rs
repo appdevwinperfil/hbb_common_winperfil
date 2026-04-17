5378cd20c852389ba61506d88e836a23ba944180
appdevwinperfil <appdev@winperfil.com>
2026-04-17 18:50:25 +0200

    Update config.rs custom server

--- src/config.rs
+++ src/config.rs
@@ -156,8 +156,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["winperfilapp.com"];
+pub const RS_PUB_KEY: &str = "jK2VYlZcFlZO4sTTUhkDCKnCc7l8xMfYPvAmZg2Vgqo=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
